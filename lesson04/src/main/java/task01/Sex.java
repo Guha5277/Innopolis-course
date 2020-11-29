52c8605772b7f20ef59cb75c0297dcc5ece2c400
Guha5277 <guhar4k@gmail.com>
2020-11-29 14:03:44 +0300

    Added check of constructor arguments

--- lesson04/src/main/java/task01/Sex.java
+++ lesson04/src/main/java/task01/Sex.java
@@ -1,12 +1,13 @@
 package task01;
 
 public class Sex implements Comparable<Sex>{
-    private final String MAN  = "MAN";
-    private final String WOMAN  = "WOMAN";
+    public final static String MAN  = "MAN";
+    public final static String WOMAN  = "WOMAN";
 
     private final String sex;
 
     public Sex(String sex) {
+        if (!sex.equals(MAN) && !sex.equals(WOMAN)) throw new IllegalArgumentException("Wrong sex argument: " + sex);
         this.sex = sex;
     }
 
