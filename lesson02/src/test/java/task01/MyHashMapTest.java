bdbb96e99cbed3224c933fc726bbe940767f9023
Guha5277 <guhar4k@gmail.com>
2020-12-01 14:52:59 +0300

    Tests init commit

---
+++ lesson02/src/test/java/task01/MyHashMapTest.java
@@ -0,0 +1,56 @@
+package task01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyHashMapTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void containsValue() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void putAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void keySet() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void entrySet() {
+    }
+}
